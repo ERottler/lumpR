4be97ad1399ddb7cc2540c91ac449578fae86d43
ERottler <rottler@uni-potsdam.de>
2022-07-13 09:06:38 +0200

    Update internal function read_raster(). This was a herlper function to avoid errors in reading raster when raster in two mapsets. This issue solved in newer GRASS versions. This function kept to ensure backward compatibility.

--- R/internals.R
+++ R/internals.R
@@ -1,13 +1,17 @@
 #internal functions
 check_raster <- function(map, argument_name="") { #check existence of raster map
+  
   cur_mapset = execGRASS("g.mapset", flags="p", intern=TRUE) #determine name of current mapset
-  if (!grepl(map, pattern = "@"))
-    map = paste0(map,"@", cur_mapset)  #add mapset name, unless already given. Otherwise, these checks may fall back on PERMANENT yielding true, but readRAST will fail later
+  
+  # #add mapset name, unless already given. Otherwise, these checks may fall back on PERMANENT yielding true, but readRAST will fail later
+  # if (!grepl(map, pattern = "@"))
+  #   map = paste0(map,"@", cur_mapset) 
 
   cmd_out <-suppressWarnings(execGRASS("r.info", map=map, intern = T, ignore.stderr = TRUE))
   
   if (!is.null(attr(cmd_out, "status")) && attr(cmd_out, "status")==1)
     stop(paste0("Raster map '", map, "' not found", ifelse(argument_name=="", ".", paste0(" (argument '", argument_name,"'). If the map is in another mapset, add '@othermapset' to the name."))))
+
 }
 
 check_vector <- function(map, argument_name="") { #check existence of vector map
@@ -17,22 +21,40 @@ check_vector <- function(map, argument_name="") { #check existence of vector map
     stop(paste0("Vector map '", map, "' not found", ifelse(argument_name=="", ".", paste0(" (argument '", argument_name,"'). If the map is in another mapset, add '@othermapset' to the name."))))
 }
 
+#read raster from GRASS-location
 read_raster <- function(raster_name) { 
-  #read raster from GRASS-location
-  #if raster exist both in local mapset and PERMANENT, force the reading of the local version, otherwise readRAST fails
-  cur_mapset = execGRASS("g.mapset", flags="p", intern=TRUE) #determine name of current mapset
-  # mapset name in R (meta symbols replaced by ".")
-  cur_mapset_r = gsub("[-+*/@.?!]", ".", cur_mapset)
-  if (!grepl(raster_name, pattern = "@")) #expand raster name because of bug in readRAST 
-  {  
-    raster_name = paste0(raster_name,"@", cur_mapset) #add mapset name, unless already given. Otherwise, strange errors may occur when the same raster exists in PERMANENT
-    name_expanded = TRUE #indicate that 
-  }  else name_expanded = FALSE
-  tmp <- readRAST(raster_name)
-  tmp <- raster(tmp)
-  if (name_expanded) #"de-expand" name, if expanded before
-    tmp@data@names = sub(x = tmp@data@names, pattern = paste0("\\.", cur_mapset_r), repl="")
-  return(tmp)
+  
+  #case 1: mapset defined in name
+  if(!grepl(raster_name, pattern = "@")){
+    
+    tmp <- read_RAST(raster_name)
+    tmp <- raster(tmp)
+    
+  }else{
+    
+    #determine name of current mapset
+    cur_mapset = execGRASS("g.mapset", flags="p", intern=TRUE)
+    
+    #case 2: raster in current mapset OR PERMANENT
+    if(raster_name %in% unlist(attributes(execGRASS("g.list", parameters = list(type = "raster"))))){
+      
+      tmp <- read_RAST(raster_name)
+      tmp <- raster(tmp)
+      
+    }
+    
+    #case 3: raster in current mapset and PERMANENT > read from current mapset
+    if(paste0(raster_name, "@", cur_mapset) %in% unlist(attributes(execGRASS("g.list", parameters = list(type = "raster"))))){
+      
+      tmp <- read_RAST(paste0(raster_name,"@", cur_mapset))
+      tmp <- raster(tmp)
+      
+    }
+    
+    return(tmp)
+    
+  }
+  
 }
 
 clean_temp_dir <- function(file_name) {
